e6c897273598878c28a1e6d70b871d008210bf89
Maksim Kanev <maks_kv@yahoo.com>
2025-08-08 20:38:37 +0300

    Fixed button behavior in job scanner

--- frontend/app/[locale]/job-scanner/page.tsx
+++ frontend/app/[locale]/job-scanner/page.tsx
@@ -172,21 +172,23 @@ export default function JobScannerPage() {
                   )}
                 </button>
                 
-                <button
-                  type="button"
-                  className="px-6 py-2.5 bg-gray-200 text-gray-800 rounded-lg font-medium hover:bg-gray-300 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center flex-1 sm:flex-none transition-colors"
-                  onClick={() => {
-                    setJobDescription('');
-                    setResult(null);
-                    setError(null);
-                  }}
-                  disabled={isLoading || (!jobDescription && !result && !error)}
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-                  </svg>
-                  {t('clear')}
-                </button>
+                {jobDescription.trim() && (
+                  <button
+                    type="button"
+                    className="px-6 py-2.5 bg-gray-200 text-gray-800 rounded-lg font-medium hover:bg-gray-300 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center flex-1 sm:flex-none transition-colors"
+                    onClick={() => {
+                      setJobDescription('');
+                      setResult(null);
+                      setError(null);
+                    }}
+                    disabled={isLoading}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                      <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                    </svg>
+                    {t('clear')}
+                  </button>
+                )}
               </div>
             </div>
 
